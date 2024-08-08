b45aae3ac06b1ed1bc2672c9f4a99c5f39a9d7b0
NIKOLAOS NEKTARIOS CHRONIS <120310095+NickChronis2004@users.noreply.github.com>
2024-08-08 19:17:31 +0300

    sensetive_info.py

--- sensetive_info.py
+++ sensetive_info.py
@@ -1,20 +1,20 @@
 # sensetive_info.py
-CHAT_GPT_API_KEY = "sk-proj-Bwx6dgrRffjAxjQcSXT5T3BlbkFJSdOHmndXXH2VINl1J61b"
+CHAT_GPT_API_KEY = " "
 
-WEBMAIL_LINK= "https://webmail.csd.uoc.gr/"
+WEBMAIL_LINK= " "
 
-GITHUB_LINK = "https://github.com/NickChronis2004?tab=repositories"
+GITHUB_LINK = " " 
 
 # Constants for credentials
-CLIENT_ID = '23457be9219b4bf3ac3ca772d46644be'
-CLIENT_SECRET = '7d7f14f186f9430995cc5bb613a90a88'
-REDIRECT_URI = 'http://localhost:8888/callback'
+CLIENT_ID = ' '
+CLIENT_SECRET = ' '
+REDIRECT_URI = ' '
 
-WEATHER_API_KEY = 'f8d727a2541dab5f12afe0dcd509f2b6'
+WEATHER_API_KEY = ' '
 
-NEWS_API_KEY = '0b2856d706cf4cbda54524ad5ce312a6'
-NEWS_API_KEY_URL = 'https://newsapi.org/v2/top-headlines'
+NEWS_API_KEY = ' '
+NEWS_API_KEY_URL = ' '
 
-VONAGE_API_KEY = '72e8a333'
-VONAGE_API_SECRET = 'nB5rZGNKI2BBU1Wn'
-PHONE_NUMBER = '306980918322'
+VONAGE_API_KEY =' '
+VONAGE_API_SECRET = ' '
+PHONE_NUMBER = ' '
