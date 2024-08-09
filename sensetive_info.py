60c8d1c698c810587c9e3ad022f5c101002b07a5
NIKOLAOS NEKTARIOS CHRONIS <120310095+NickChronis2004@users.noreply.github.com>
2024-08-09 21:10:54 +0300

    sensetive_info.py

--- sensetive_info.py
+++ sensetive_info.py
@@ -1,4 +1,6 @@
 # sensetive_info.py
+
+#visit https://platform.openai.com/api-keys
 CHAT_GPT_API_KEY = " "
 
 WEBMAIL_LINK= " "
@@ -6,15 +8,22 @@ WEBMAIL_LINK= " "
 GITHUB_LINK = " " 
 
 # Constants for credentials
+
+#visit https://developer.spotify.com/documentation/web-api
 CLIENT_ID = ' '
 CLIENT_SECRET = ' '
 REDIRECT_URI = ' '
 
+#visit https://openweathermap.org/api
 WEATHER_API_KEY = ' '
 
+#visit https://newsapi.org/
 NEWS_API_KEY = ' '
 NEWS_API_KEY_URL = ' '
 
+#visit https://www.vonage.com/communications-apis/sms/
 VONAGE_API_KEY =' '
 VONAGE_API_SECRET = ' '
+
+#your phone number
 PHONE_NUMBER = ' '
